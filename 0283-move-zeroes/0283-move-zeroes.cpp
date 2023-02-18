348fd967eeed5b7a221bda319947d9552e94836d
ANKIT GHOSAL <106977703+ankitbhoi@users.noreply.github.com>
2023-02-18 09:29:51 +0530

    Time: 28 ms (51.07%), Space: 19.2 MB (65.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int fp=0;
+        int sp=0;
+        while(sp<=nums.size()-1){
+            if(nums[fp]==0){ //0
+                if(nums[sp]!=0){  //1
+                    swap(nums[fp],nums[sp]);
+                    fp++;
+                    sp++;
+                }
+                else{     //0
+                    sp++;
+                }
+            }
+            else if(nums[sp]!=0){ //1  //1
+                    fp++;
+                    sp++;
+            }
+        }
+    }
+};
