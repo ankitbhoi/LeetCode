ddc4ec741560b17d97a95cb18c2698b44a6a145a
ANKIT GHOSAL <106977703+ankitbhoi@users.noreply.github.com>
2023-03-30 18:48:13 +0530

    Time: 3 ms (72.23%), Space: 8 MB (30.19%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void perm(int ind,vector<int>&ds,vector<int>&nums,int freq[]){
+        if(ind==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){   //if no element is taken or marked in freq array
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                perm(ind+1,ds,nums,freq);
+                ds.pop_back();
+                freq[i]=0;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>ds;
+        //int s=nums.size();
+        int freq[nums.size()];
+        for(int i=0;i<nums.size();i++){
+            freq[i]=0;
+        }
+        perm(0,ds,nums,freq);
+        return ans;
+    }
+};
