4e451cd69a85e4e73edda01bd00574b03e9ca76c
ANKIT GHOSAL <106977703+ankitbhoi@users.noreply.github.com>
2023-04-11 20:10:06 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (16.16%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    vector<string>ans;
-    void combo(int ind,string ds,map<char,string>mp,string digits){
-        if(ind==digits.size()){
-            ans.push_back(ds);
-            return;
-        }
-        
-        string s1= mp[digits[ind]]; //digits[0]=2 and 2 is mapped to abc;
-        for(int i=0;i<s1.length();i++){
-            ds.push_back(s1[i]);
-            combo(ind+1,ds,mp,digits);
-            ds.pop_back();
-        }
-        
-    }
+    vector<string> a;
     vector<string> letterCombinations(string digits) {
-        if(digits.size()==0){
-            return ans;
-        }
-        map<char,string>mp;
-        mp.insert({'2',"abc"});
-        mp.insert({'3',"def"});
-        mp.insert({'4',"ghi"});
-        mp.insert({'5',"jkl"});
-        mp.insert({'6',"mno"});
-        mp.insert({'7',"pqrs"});
-        mp.insert({'8',"tuv"});
-        mp.insert({'9',"wxyz"});
-        
-        combo(0,"",mp,digits);
-        
-        return ans;
-    }
+    if(digits.size()==0)
+        return a;
+    map<int,string> m;
+    string b="";
+    m[2]="abc";
+    m[3]="def";
+    m[4]="ghi";
+    m[5]="jkl";
+    m[6]="mno";
+    m[7]="pqrs";
+    m[8]="tuv";
+    m[9]="wxyz";
+    solve(b,m,0,digits);
+    return a;
+  }
+  void solve(string b,map<int,string>& m,int j,string digits)
+  {
+      if(j==digits.size())
+      {
+          a.push_back(b);
+          return;
+      }
+      map<int,string>::iterator itr=m.find(digits[j]-'0');
+      string str=itr->second;
+      // cout<<itr->first<<endl;
+      int k=j+1;
+      // string str= m[digits[j]-'0'];
+      for(int i=0;i<str.size();i++)
+      {
+          b.push_back(str[i]);
+          solve(b,m,k,digits);
+          b.pop_back();
+      }
+  }
 };
