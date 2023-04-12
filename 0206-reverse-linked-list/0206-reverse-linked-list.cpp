117a6b25c1d97d3957719a62c57d223c04d3932f
ANKIT GHOSAL <106977703+ankitbhoi@users.noreply.github.com>
2023-04-13 01:34:42 +0530

    Time: 3 ms (95.43%), Space: 8.6 MB (11.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,17 +11,34 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode *prev=NULL;
-        ListNode *curr=head;
-        ListNode *next;
-        while(curr){
-            next=curr->next;
-            curr->next=prev;
-            
-            prev=curr;
-            curr=next;
+        if(head==NULL||head->next==NULL){
+            return head;
         }
-        head=prev;
-        return head;
+        ListNode *nnode=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return nnode;
     }
+    
+    // basically idhr 1->2->3->4->NULL pe nnode pe 4 return hoga and head k value 3 rhega pehele se to uske bad tmko 3->next->next that is (4->NULL) ko 4->3 kr derhe ho aur 3->NULL kr derhe ho.
+    //     phir next call me phirse 2->3->4 ke case me 3->2 ko point krega and 2->null ko aur 4->3 pehele se hi kia hua hai pichle call se
+    //         finally tmko nnode return kr dena hai jo ki suru pe hi 4 tha aur har bar 4 hi arha hai..
 };
+// iterative
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode *prev=NULL;
+//         ListNode *curr=head;
+//         ListNode *next;
+//         while(curr){
+//             next=curr->next;
+//             curr->next=prev;
+            
+//             prev=curr;
+//             curr=next;
+//         }
+//         head=prev;
+//         return head;
+//     }
+// };
