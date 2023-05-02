1ff08bc49a1911ad869ceb0b0cf04feb0e8aa40c
ANKIT GHOSAL <106977703+ankitbhoi@users.noreply.github.com>
2023-05-02 22:26:03 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (22.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int climbWays(int ind,vector<int>&dp,int n){
-        if(ind==n){
-            return 1;
-        }
-        if(ind==n-1){
-            return 1;
-        }
-        if(dp[ind]!=-1) return dp[ind];
-        return dp[ind]=climbWays(ind+1,dp,n)+climbWays(ind+2,dp,n);
-    }
+    // int climbWays(int ind,vector<int>&dp,int n){
+    //     if(ind==n){
+    //         return 1;
+    //     }
+    //     if(ind==n-1){
+    //         return 1;
+    //     }
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     return dp[ind]=climbWays(ind+1,dp,n)+climbWays(ind+2,dp,n);
+    // }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return climbWays(0,dp,n);
+        dp[n]=1;
+        dp[n-1]=1;
+        
+        for(int ind=n-2;ind>=0;ind--){
+            dp[ind]=dp[ind+1]+dp[ind+2];
+        }
+        
+        return dp[0];
     }
 };
