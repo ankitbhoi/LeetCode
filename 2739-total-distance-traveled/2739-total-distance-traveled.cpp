4efe139f33b3ece1602ea8821fb56c9e3006e2a6
ANKIT GHOSAL <106977703+ankitbhoi@users.noreply.github.com>
2023-06-18 21:31:07 +0530

    Time: 20 ms (55.56%), Space: 5.9 MB (88.89%) - LeetHub

--- 2739-total-distance-traveled/2739-total-distance-traveled.cpp
+++ 2739-total-distance-traveled/2739-total-distance-traveled.cpp
@@ -3,11 +3,11 @@ public:
     int distanceTraveled(int mainTank, int additionalTank) {
         int distance=0;
         for(auto i=1;i<=mainTank;i++){
+            distance+=10;
             if(additionalTank!=0 && i%5==0){   
                 additionalTank-=1;
                 mainTank+=1;
             }
-            distance+=10;
         }
         return distance;
     }
