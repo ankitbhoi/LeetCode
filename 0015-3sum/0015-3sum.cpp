60d1cf8f3fd5d1121c0e97cf084eea35ec7468c8
ANKIT GHOSAL <106977703+ankitbhoi@users.noreply.github.com>
2023-07-05 19:01:31 +0530

    Time: 145 ms (83.77%), Space: 24.1 MB (70.93%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        for(int i=0;i<nums.size()-2;i++){
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                int low=i+1,high=nums.size()-1,sum=0-nums[i];
-                while(low<high){
-                    if(nums[low]+nums[high]==sum){
-                        vector<int>temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[low]);
-                        temp.push_back(nums[high]);
-                        ans.push_back(temp);
-                        
-                        while(low<high && nums[low]==nums[low+1]){
-                            low++;
-                        }
-                        while(low<high && nums[high]==nums[high-1]){
-                            high--;
-                        }
-                        low++;
-                        high--;
-                    }
-                    else if(nums[low]+nums[high]<sum){
-                        low++;
-                    }
-                    else{
-                        high--;
-                    }
+        vector<vector<int>> result;
+        if (nums.size() < 3)
+            return result;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = nums.size() - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    
+                    // Skip duplicate values for the second and third elements
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-        return ans;
+
+        return result;
     }
 };
